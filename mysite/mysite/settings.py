1faf418e6b7340f90064634cae0bcc88de69e43b
Zim Gong <zimgong@umich.edu>
2023-12-03 18:18:53 -0500

    use MySQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,11 +89,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ZimG386$ads',
+        'USER': 'ZimG386',
+        'PASSWORD': 'A8jwppepipy!',
+        'HOST': 'ZimG386.mysql.pythonanywhere-services.com',
+         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
